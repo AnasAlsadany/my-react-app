9367f88f4ab4ae82003c13c11e58f2ee34191e1f
Your Name <anas.elsadany20@gmail.com>
2025-04-15 13:17:04 +0200

    fixed issues

--- src/sections/FeatureShowcase/InvoiceCard.tsx
+++ src/sections/FeatureShowcase/InvoiceCard.tsx
@@ -1,60 +1,3 @@
-// import React from "react";
-// import { ListGroup, Badge } from "react-bootstrap";
-// import { useTheme } from "next-themes";
-// import SharedCard from "../../@shared/SharedCard";
-
-// type InvoiceFile = {
-//   name: string;
-//   color: string;
-// };
-
-// const invoiceFiles: InvoiceFile[] = [
-//   { name: "John Client_download.Pdf", color: "pink" },
-//   { name: "Michele Leos_download.Pdf", color: "blue" },
-//   { name: "John Smith_download.Pdf", color: "lightgreen" },
-// ];
-
-// const InvoiceCard: React.FC = () => {
-//   const { theme } = useTheme();
-//   const isDark = theme === "dark";
-
-//   return (
-//     <SharedCard title="$3453.00" description="MonthlyInvoice">
-//       <div className="d-flex justify-content-between align-items-center mb-3">
-//         <div className="d-flex align-items-center gap-3 mx-3">
-//           <div className="card"> 
-
-//           </div>
-//         </div>
-
-//         <ListGroup variant="flush" style={{ width: "100%" }}>
-//           {invoiceFiles.map((file, index) => (
-//             <ListGroup.Item
-//               key={index}
-//               className={`d-flex justify-content-between align-items-center px-0 ${
-//                 isDark ? "bg-dark text-white" : "bg-white text-dark"
-//               }`}
-//             >
-//               <div className="d-flex align-items-center gap-2">
-//                 <Badge
-//                   style={{
-//                     backgroundColor: file.color,
-//                     width: "25px",
-//                     height: "25px",
-//                     borderRadius: "50%",
-//                   }}
-//                 />
-//                 <span className="small">{file.name}</span>
-//               </div>
-//             </ListGroup.Item>
-//           ))}
-//         </ListGroup>
-//       </div>
-//     </SharedCard>
-//   );
-// };
-
-// export default InvoiceCard;
 import React from 'react';
 import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -95,7 +38,7 @@ const InvoiceCard: React.FC = () => {
     >
       <Row>
         <Col md={12}>
-        <h4>Real-time accounting at your fingertips.</h4>
+        <h6>Real-time accounting at your fingertips.</h6>
           <p style={{ fontSize: '1.2rem', color: textColor }}>
             Take the pain out of book keeping! Wave goodbye to mountains of
             paperwork and endless email reminders. There`s now a new way of
